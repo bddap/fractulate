7842b70213011c79cb73b1a2362bb5a1aa04f81d
Andrew Dirksen <andrew@dirksen.com>
2024-11-18 17:14:38 -0800

    recursive fractal

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
 use nalgebra::{Matrix4, Point3, Vector3};
-use rand::{seq::SliceRandom, SeedableRng};
+use rand::{seq::SliceRandom, Rng, SeedableRng};
 use std::io::{Cursor, Read};
 
 use stl_io::{read_stl, write_stl, Normal, Triangle, Vertex};
 
 fn main() -> Result<()> {
     let mesh = load()?;
-    let mesh = transform(mesh);
+    let mesh = main_transform(mesh);
     save(mesh)?;
     Ok(())
 }
@@ -43,26 +43,53 @@ fn save(mesh: Vec<[Vector3<f32>; 3]>) -> Result<()> {
     Ok(())
 }
 
-fn transform(triangles: Vec<[Vector3<f32>; 3]>) -> Vec<[Vector3<f32>; 3]> {
-    let mut mesh = triangles.clone();
-
+fn main_transform(triangles: Vec<[Vector3<f32>; 3]>) -> Vec<[Vector3<f32>; 3]> {
     let mut rng = rand_xoshiro::Xoshiro256StarStar::seed_from_u64(0);
+    let mut ret = triangles.clone();
+    ret.extend(growths(&mut rng, &triangles, 3));
+    ret
+}
 
-    for _ in 0..30 {
-        let Some(triangle) = triangles.choose(&mut rng) else {
+fn growths<R: Rng>(
+    rng: &mut R,
+    base_model: &[[Vector3<f32>; 3]],
+    depth: usize,
+) -> Vec<[Vector3<f32>; 3]> {
+    let Some(next_depth) = depth.checked_sub(1) else {
+        return Vec::new();
+    };
+
+    let num_children = 3;
+    let child_scale = 0.5;
+
+    let mut ret = Vec::new();
+
+    for _ in 0..num_children {
+        let Some(triangle) = base_model.choose(rng) else {
             return Vec::new();
         };
-        let transformation = place_on_triangle(*triangle) * Matrix4::new_scaling(0.5);
-        mesh.extend(triangles.iter().map(|triangle| {
-            triangle.map(|v| {
-                let v: Point3<f32> = v.into();
-                let v = transformation.transform_point(&v);
-                v.coords
-            })
-        }));
+        let transformation = place_on_triangle(*triangle) * Matrix4::new_scaling(child_scale);
+        ret.extend(transform(base_model.to_vec(), transformation));
+        ret.extend(transform(
+            growths(rng, base_model, next_depth),
+            transformation,
+        ));
     }
 
-    mesh
+    ret
+}
+
+fn transform(
+    triangles: Vec<[Vector3<f32>; 3]>,
+    transformation: Matrix4<f32>,
+) -> Vec<[Vector3<f32>; 3]> {
+    let mut triangles = triangles;
+    for triangle in triangles.iter_mut() {
+        for v in triangle.iter_mut() {
+            *v = transformation.transform_point(&(*v).into()).coords;
+        }
+    }
+    triangles
 }
 
 fn get_normal(face: &[Vector3<f32>; 3]) -> nalgebra::Vector3<f32> {
